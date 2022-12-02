8c9d6037f6694290147d79027dd17a9c11316853
cocahonka <cocahonka@gmail.com>
2022-12-02 20:01:32 +0300

    dynamic header in opening screen with urls

--- apps/opening/views.py
+++ apps/opening/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+header_menu = [
+    {"title": "войти", "url_name": "login"},
+    {"title": "про нас", "url_name": "#"},
+    {"title": "скачать", "url_name": "#"},
+    {"title": "блог", "url_name": "#"},
+    {"title": "q&a", "url_name": "#"},
+]
+
 
 def index(request: HttpRequest):
-    return render(request, "opening/index.html")
+    context = {
+        "header_menu": header_menu,
+        "title": "Чам чакет",
+    }
+    return render(request, "opening/index.html", context=context)
+
+
+def login(request: HttpRequest):
+    return HttpResponse("Войти")
