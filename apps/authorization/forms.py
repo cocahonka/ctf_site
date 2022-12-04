68ea43f98479e0f4a0e57475b401a5e45b5deaf0
cocahonka <cocahonka@gmail.com>
2022-12-04 03:51:38 +0300

    sync profile creation with the user creation

--- apps/authorization/forms.py
+++ apps/authorization/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+from .models import *
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -9,3 +11,7 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+    def save(self, *args, **kwargs):
+        user = super(UserRegisterForm, self).save(*args, **kwargs)
+        Profile.objects.create(user=user)
