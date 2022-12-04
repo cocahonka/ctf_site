34e9271665e5b90a6bf55a808e07aa88866ef9d8
cocahonka <cocahonka@gmail.com>
2022-12-04 03:04:14 +0300

    inject auth logic

--- apps/authorization/views.py
+++ apps/authorization/views.py
@@ -1,2 +1,23 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"{username} Ваш аккаунт создан: можно войти на сайт.")
+            return redirect("login")
+    else:
+        form = UserRegisterForm()
+    return render(request, "authorization/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    return render(request, "authorization/profile.html")
